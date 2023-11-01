f7f1d36f524f6b0529fe560e480638d3c3519a8c
tranchi0120 <tranchi20012000@gmail.com>
2023-11-01 22:49:23 +0700

    animation

--- js/main.js
+++ js/main.js
@@ -22,3 +22,23 @@ const scrollTop = () => {
 };
 
 window.addEventListener("scroll", scrollTop);
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr__top = ScrollReveal({
+  origin: "bottom",
+  distance: "30px",
+  duration: 2000,
+  reset: false,
+});
+
+sr__top.reveal(
+  `.hero,.banner-logo,
+  .banner-title,.banner-text,
+  .banner-btn,.banner-box,.hero-image,
+  .listSection,.pictureSection,.utilities-heading,.utilities-content,.utilities-box,
+  .slogan-title,.slogan-content,.slogan-btn,.contact-title,.contact-text,.contact-input,
+  .contact-btn,.footer `,
+  {
+    interval: 200,
+  }
+);
